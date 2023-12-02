c7248e7feae40d9d9a288ff4d298199b80a7383c
jhv9096 <69939024+jhv9096@users.noreply.github.com>
2023-12-02 18:46:42 -0500

    Setting up data retrieval in ReportingStructure
    Adding methods to ReportingStructure to get and set the Employee, as well as to get the number of reports. Also includes a method to update the count of reports every time the system requests the report count.

--- src/main/java/com/mindex/challenge/data/ReportingStrcuture.java
+++ src/main/java/com/mindex/challenge/data/ReportingStrcuture.java
@@ -3,4 +3,45 @@ package com.mindex.challenge.data;
 import com.mindex.challenge.data.Employee;
 
 public class ReportingStrcuture {
+
+    //An employee who we need to number of reports for.
+    private Employee employee;
+
+    /*
+    The total number of reports this employee receives.
+    This will include reports sent to those whom this employee receives reports from.
+     */
+    private int numberOfReports;
+
+    /*
+    Returns the employee as an Employee object.
+     */
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public int getNumberOfReports() {
+        updateNumberOfReports(); //We run this everytime to ensure we return an up-to-date-value.
+        return numberOfReports;
+    }
+
+    protected void updateNumberOfReports() {
+        numberOfReports = 0;
+        //If the employee we are looking at does not have any direct reports.
+        if(employee.getDirectReports() == null || employee.getDirectReports().size() == 0) {
+            numberOfReports = 0; //We don't change teh value at all
+        }
+        else {
+            for(Employee reportee : employee.getDirectReports()) {
+                //Add 1 report for the current employee, as well as however many reports
+                //they receive as well.
+                numberOfReports += (1 + reportee.getDirectReports().size());
+            }
+        }
+
+    }
 }
