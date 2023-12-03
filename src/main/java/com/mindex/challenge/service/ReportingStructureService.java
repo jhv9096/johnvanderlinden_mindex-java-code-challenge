b26a02715e26eddb3910891abd75bb2e75bc7c22
jhv9096 <69939024+jhv9096@users.noreply.github.com>
2023-12-02 21:29:56 -0500

    Adding more REST files
    Working on adding more to the RESTful implementation.

---
+++ src/main/java/com/mindex/challenge/service/ReportingStructureService.java
@@ -0,0 +1,12 @@
+package com.mindex.challenge.service;
+
+import com.mindex.challenge.data.ReportingStructure;
+
+public interface ReportingStructureService {
+
+    ReportingStructure create();
+
+    ReportingStructure read();
+
+    ReportingStructure update();
+}
---
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -0,0 +1,14 @@
+package com.mindex.challenge.service.impl;
+
+
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.UUID;
+
+public class ReportingStructureServiceImpl {
+
+}
