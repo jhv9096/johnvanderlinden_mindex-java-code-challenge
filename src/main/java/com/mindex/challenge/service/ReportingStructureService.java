f2cb2872676ee8e6b28bb4fa03db75ec51c0a908
jhv9096 <69939024+jhv9096@users.noreply.github.com>
2023-12-05 19:57:06 -0500

    Adding more REST functionality
    Updated ReportingStructure to have REST API calls using Employee as the basis.

--- src/main/java/com/mindex/challenge/service/ReportingStructureService.java
+++ src/main/java/com/mindex/challenge/service/ReportingStructureService.java
@@ -1,12 +1,13 @@
 package com.mindex.challenge.service;
 
 import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.data.Employee;
 
 public interface ReportingStructureService {
 
-    ReportingStructure create();
+    ReportingStructure create(ReportingStructure reportingStructure, Employee employee);
 
-    ReportingStructure read();
+    ReportingStructure read(String employeeId);
 
-    ReportingStructure update();
+    ReportingStructure update(ReportingStructure reportingStructure, Employee employee);
 }
--- src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -1,6 +1,8 @@
 package com.mindex.challenge.service.impl;
 
 
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.dao.ReportingStructureRepository;
 import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.ReportingStructureService;
@@ -17,6 +19,35 @@ public class ReportingStructureServiceImpl implements ReportingStructureService 
 
     @Autowired
     private ReportingStructureRepository reportingStructureRepository;
+
+    @Override
+    public ReportingStructure create(ReportingStructure reportingStructure, Employee employee) {
+        LOG.debug("Creating reporting structure [{}] for employee [{}]", reportingStructure, employee);
+
+        reportingStructure.setEmployee(employee);
+        reportingStructureRepository.insert(reportingStructure);
+
+        return reportingStructure;
+    }
+
+    @Override
+    public ReportingStructure read(String employeeId) {
+        LOG.debug("Reading reporting structure whose employee has id [{}]", employeeId);
+
+        ReportingStructure reportingStructure = reportingStructureRepository.findByEmployeeId(employeeId);
+        if (reportingStructure == null) {
+            throw new RuntimeException("Invalid employeeId: " + employeeId);
+        }
+
+        return reportingStructure;
+    }
+
+    @Override
+    public ReportingStructure update(ReportingStructure reportingStructure, Employee employee) {
+        LOG.debug("Updating reporting structure [{}] for employee [{}]", reportingStructure, employee);
+
+        return reportingStructureRepository.save(reportingStructure);
+    }
 
 
 }
